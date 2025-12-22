ad92affb6e085c2167a2114815776ad84b11cfd3
Janaina <janainafrv@gmail.com>
2025-12-22 20:16:18 -0300

    Testando apply

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,15 @@ variable "sns_topic_exists" {
   type    = bool
   default = false
 }
+
+variable "tf_action" {
+  type    = string
+  description = "Ação do Terraform, ex: apply ou destroy"
+  default = "apply"
+}
+
+variable "sns_arn" {
+  type        = string
+  description = "ARN do tópico SNS"
+  default     = ""
+}
