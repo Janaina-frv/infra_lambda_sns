15f86a28f96f777019af151fee77d699a606d97e
Janaina <janainafrv@gmail.com>
2025-12-22 20:51:15 -0300

    Testando destroy

--- infra/iam.tf
+++ infra/iam.tf
@@ -8,3 +8,22 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
   role       = data.aws_iam_role.lambda_exec_role.id
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-java-exec-role-manual"
+
+  description = "IAM Role para execução de funções Lambda em Java com permissão de publicar no SNS"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
