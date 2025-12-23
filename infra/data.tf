f24969d272c20a80241fa865190530328ac953a3
Janaina <janainafrv@gmail.com>
2025-12-22 21:20:51 -0300

    Testando destroy

--- infra/data.tf
+++ infra/data.tf
@@ -1,7 +1,3 @@
-data "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-java-exec-role-manual"
-}
-
 data "aws_sns_topic" "feedback_urgente" {
   count = var.sns_topic_exists ? 1 : 0
   name  = "feedback_urgente-sns"
