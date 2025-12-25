2d2364abf727f4f72773bccc61afbbdf3d0aaf7d
Janaina <janainafrv@gmail.com>
2025-12-25 19:41:17 -0300

    Testando destroy

--- infra/iam.tf
+++ infra/iam.tf
@@ -17,13 +17,31 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
+#####################################
+# Policy para Lambda
+#####################################
+
 resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda-sns-publish-policy"
   description = "Permite Lambda publicar mensagens no SNS"
   policy      = file("${path.module}/policy/lambda.json")
 }
 
+#####################################
+# Anexos de Policy para Lambda
+#####################################
+
 resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
