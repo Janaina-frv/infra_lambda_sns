05075aeaaf83de8c0e4a6bb7b36a5b26d85601b2
Janaina <janainafrv@gmail.com>
2026-01-05 20:22:28 -0300

    Testando apply

--- lambda/src/test/java/br/com/lambda/LambdaHandlerTest.java
+++ lambda/src/test/java/br/com/lambda/LambdaHandlerTest.java
@@ -1,17 +1,17 @@
-package br.com.lambda;
-
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.test.junit.QuarkusTest;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-
-@QuarkusTest
-class LambdaHandlerTest {
-    @Test
-    void testSimpleLambdaSuccess() throws Exception {
-
-    }
-
-}
+//package br.com.lambda;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import io.quarkus.test.junit.QuarkusTest;
+//
+//import static io.restassured.RestAssured.given;
+//import static org.hamcrest.CoreMatchers.containsString;
+//
+//@QuarkusTest
+//class LambdaHandlerTest {
+//    @Test
+//    void testSimpleLambdaSuccess() throws Exception {
+//
+//    }
+//
+//}
--- lambda/src/test/java/br/com/lambda/LambdaHandlerTestIT.java
+++ lambda/src/test/java/br/com/lambda/LambdaHandlerTestIT.java
@@ -1,9 +1,9 @@
-package br.com.lambda;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class LambdaHandlerTestIT extends LambdaHandlerTest {
-
-    // Execute the same tests but in native mode.
-}
+//package br.com.lambda;
+//
+//import io.quarkus.test.junit.QuarkusIntegrationTest;
+//
+//@QuarkusIntegrationTest
+//public class LambdaHandlerTestIT extends LambdaHandlerTest {
+//
+//    // Execute the same tests but in native mode.
+//}
