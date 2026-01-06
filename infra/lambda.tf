a2f1b6b531e4bb802cf423439d9ffc0b883c67de
Janaina <janainafrv@gmail.com>
2026-01-05 21:48:51 -0300

    Testando destroy

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "hello_lambda" {
   handler       = "br.com.lambda.NotificationLambda::handleRequest"
   runtime       = "java17"
 
-  filename      = "${path.module}/build/lambda/ms-notification-1.0.0-SNAPSHOT.jar"
+  filename      = "${path.module}/build/lambda/ms-notification.jar"
 
   memory_size = 512
   timeout     = 10
